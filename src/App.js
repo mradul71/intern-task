10c2b66b3a3ac1bedccd9ffed5345b05cbbcea71
mradul agarwal <mradulagarwal790@gmail.com>
2021-12-29 15:55:45 +0530

    AfasF

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
-
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+  
 function App() {
+  
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Route exact path='/register' component={Register} />
+      <Route exact path='/login' component={Login} />
+     </div>
+     </Router>
   );
 }
-
+  
 export default App;
